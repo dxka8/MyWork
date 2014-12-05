605b4de4040b4e4f3b1cc8acea51ab4fb3183a47
2558065584@qq.com <2558065584@qq.com>
2014-12-05 16:43:44 +0800

    2014/12/5 pm2

---
+++ MY.Web.Admin/Admin.Demo.Core.Data/Initialize/SampleData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Admin.Demo.Core.Data.Context;
+using Admin.Demo.Core.Models.Account;
+
+namespace Admin.Demo.Core.Data.Initialize
+{
+    public class SampleData : CreateDatabaseIfNotExists<DemoDbContext>
+    {
+        protected override void Seed(DemoDbContext context)
+        {
+            var members = new List<Member>()
+            {
+                new Member(){UserName = "Admin",Password ="123456",NickName = "曾凡雨",Email = "258065584@qq.com"},
+                new Member(){UserName = "zfy",Password ="123456",NickName = "曾凡雨",Email = "258065584@qq.com"}
+            };
+            DbSet<Member> memberSet = context.Set<Member>();
+            members.ForEach(m => memberSet.Add(m));
+            context.SaveChanges();          
+        }
+    }
+}
