fe8ccbc3baec347a9715bc0db3180669d9898226
2558065584@qq.com <2558065584@qq.com>
2014-12-19 16:42:18 +0800

    2014/12/19 pm3 t4

---
+++ MY.Web.Admin/Admin.Demo.Core.Data/Configurations/Account/MemberExtendConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Demo.Core.Data.Configurations.Account
+{
+    partial class MemberExtendConfiguration
+    {
+        partial void MemberExtendConfigurationAppend()
+        {
+            HasRequired(m => m.Member).WithOptional(n => n.Extend);
+        }
+    }
+}
